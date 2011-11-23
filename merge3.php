bcaa2feb8e2cbecb5c4c4b8c507df2d078299d61
Luke Palmer <lrpalmer@gmail.com>
2011-11-23 06:46:12 +0000

    Made a merge3 function.  May I say, woot?

--- merge3.php
+++ merge3.php
@@ -1,7 +1,7 @@
 <?php
 
 class DiffModule {
-    function lcs($R, $C) {
+    function diff($R, $C) {
         // see en.wikipedia.org/wiki/Longest_common_subsequence for algorithm
         
         // Each element in the table is a 2-array: [length, direction].
@@ -14,14 +14,11 @@ class DiffModule {
         $UP     = 2;  //  (dec row)
         $BOTH   = 3;
 
-        // We do not need to fill in direction for the -1 entries
-        // because direction is only used during reconstruction,
-        // which will terminate at index 0.
         for ($i = 0; $i < count($R); $i++) {
-            $table[$i][-1][$LENGTH] = 0;
+            $table[$i][-1] = array(0, $UP);
         }
         for ($j = 0; $j < count($C); $j++) {
-            $table[-1][$j][$LENGTH] = 0;
+            $table[-1][$j] = array(0, $LEFT);
         }
         $table[-1][-1] = array(0,0);
 
@@ -41,7 +38,7 @@ class DiffModule {
                         $table[$i][$j] = array($lenup, $UP);
                     }
                     else {
-                        $table[$i][$j] = array($lenleft, $UPLEFT);
+                        $table[$i][$j] = array($lenleft, $BOTH);
                     }
                 }
             }
@@ -50,36 +47,103 @@ class DiffModule {
         // Reconstruct.
         $i = count($R)-1;
         $j = count($C)-1;
-        $rix = $table[$i-1][$j-1][$LENGTH];
-        while ($i != -1 && $j != -1) {
+        $rix = 0;
+        while ($i != -1 || $j != -1) {
             $elem = $table[$i][$j];
             $dir = $elem[$DIRECTION];
             if ($dir == $UPLEFT) {
-                $result[$rix] = $R[$i]; // == $C[$j]
-                $i--; $j--; $rix--;
+                $result[$rix++] = array(" ", $R[$i]); // == $C[$j]
+                $i--; $j--;
             }
             else if ($dir == $LEFT) {
+                $result[$rix++] = array("+", $C[$j]);
                 $j--;
             }
-            else if ($dir == $UP) {
-                $i--;
-            }
-            else if ($dir == $BOTH) {
-                // uhhh... pick one I guess.  We could also do some callback bullshit
-                // to return all results, but we'll se if we need that.
-                $i--;
+            else if ($dir == $UP || $dir == $BOTH) {  // if both we just pick one I guess...
+                $result[$rix++] = array("-", $R[$i]);
+                $i--; 
             }
         }
 
-        return $result;
+        return $this->reverse($result);
     }
 
+    function reverse($a) {
+        for ($i = count($a)-1, $j=0; $i >= 0; $i--, $j++) {
+            $b[$j] = $a[$i];
+        }
+        return $b;
+    }
+
+    // Override this function for different line compare modes; eg. whitespace-insensitive.
     function compare($x, $y) {
         return $x == $y;
     }
-}
 
-$mod = new DiffModule();
-var_dump($mod->lcs(array("S", "p", "o", "o", "k"), array("S", "o", "o", "l", "k")));
+    function merge3($orig, $left, $right) {
+        $ldiff = $this->diff($orig, $left);
+        $rdiff = $this->diff($orig, $right);
+
+        var_dump($ldiff);
+        var_dump($rdiff);
+
+        $oi = $li = $ri = $zi = 0;
+        while ($oi < count($orig) || $li < count($ldiff) || $ri < count($rdiff)) {
+            if ($li < count($ldiff)) {
+                $lstat = $ldiff[$li][0];
+                $ltext = $ldiff[$li][1];
+            }
+            else {
+                $lstat = " ";
+                $ltext = null;
+            }
+
+            if ($ri < count($rdiff)) {
+                $rstat = $rdiff[$ri][0];
+                $rtext = $rdiff[$ri][1];
+            }
+            else {
+                $rstat = " ";
+                $rtext = null;
+            }
+
+            echo("$lstat$ltext\n");
+            echo("$rstat$rtext\n");
+            echo("----\n");
+            
+            switch ($lstat.$rstat) {
+                case "  ": 
+                    $result[$zi] = array("orig", $orig[$oi]);
+                    $zi++; $oi++; $li++; $ri++;
+                    break;
+                case " -":
+                case "- ":
+                case "--":
+                    $oi++; $li++; $ri++;
+                    break;
+                case "+ ":
+                    $result[$zi] = array("left", $ltext);
+                    $zi++; $li++;
+                    break;
+                case " +":
+                    $result[$zi] = array("right", $rtext);
+                    $zi++; $ri++;
+                    break;
+                case "++":
+                    if ($this->compare($ltext, $rtext)) {
+                        $result[$zi] = array("both", $ltext);
+                    }
+                    else {
+                        $result[$zi] = array("conflict", $ltext, $rtext);
+                    }
+                    $zi++; $li++; $ri++;
+                    break;
+                default:
+                    throw new Exception("Missed something: [$lstat][$rstat]");
+            }
+        }
+        return $result;
+    }
+}
 
 ?>
