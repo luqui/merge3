d7a7794413543e0229ce09d064bbaa68480f3973
Luke Palmer <lrpalmer@gmail.com>
2011-11-23 07:27:59 +0000

    Added some demo pages.

---
+++ demo.php
@@ -0,0 +1,27 @@
+<html>
+ <head>
+  <title>Merge3 demo</title>
+ </head>
+ <body>
+  <h1>Merge3 demo</h1>
+  <form action="merge3.php" method="post">
+  <table>
+   <tr>
+    <td>
+     <h3>Left</h3>
+     <textarea name="left" rows="30"></textarea>
+    </td>
+    <td>
+     <h3>Orig</h3>
+     <textarea name="orig" rows="30"></textarea>
+    </td>
+    <td>
+     <h3>Right</h3>
+     <textarea name="right" rows="30"></textarea>
+    </td>
+   </tr>
+  </table>
+  <input type="submit" />
+  </form>
+ </body>
+</head>
---
+++ merge3.php
@@ -0,0 +1,14 @@
+<html>
+ <head>
+  <title>Merged output</title>
+ </head>
+ <body>
+  <h3>Orig</h3>
+  <pre><?php echo $_POST['orig'] ?></pre>
+  <h3>Left</h3>
+  <pre><?php echo $_POST['left'] ?></pre>
+  <h3>Right</h3>
+  <pre><?php echo $_POST['right'] ?></pre>
+ </body>
+</html>
+   
